4111d916d7a3d37068606147077dd62c122a7d69
Schromeo <spouringrainc@gmail.com>
2023-11-05 00:56:58 -0700

    Initial Commit

---
+++ PawPalDemo/LoginScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LoginScreen.swift
+//  PawPalDemo
+//
+//  Created by Schromeo on 11/5/23.
+//
+
+import SwiftUI
+
+struct LoginScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginScreen()
+}
---
+++ PawPalDemo/LoginScreen/LoginView.swift
@@ -0,0 +1,25 @@
+//
+//  LoginView.swift
+//  PawPalDemo
+//
+//  Created by Schromeo on 11/5/23.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+    var labelTitle: UILabel!
+    
+    var tfEmail: UITextField!
+    var tfPassword: UITextField!
+    
+    override init(frame: CGRect) {
+        <#code#>
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
